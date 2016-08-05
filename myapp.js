e71c2b01c52e147a3b80ffb2d22acae561e6c08c
Ken Lee <ken.lee@nokia.com>
2016-08-05 20:29:03 +0800

    Initial commit.

---
+++ myapp.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var module = angular.module('nokiaclick', []);
+
+module.component('nokiaClickNav', {
+	templateUrl: 'navbar.html'
+});
+
+module.component('reports', {
+	templateUrl: 'reports.html'
+})
